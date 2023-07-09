fe7c2481cc4a19ac852e3418dec6f19080b5d5a9
Piotr Gebski <gebskipiotr@gmail.com>
2023-07-09 17:26:11 +0200

    Added basic authentication using rest_framework, added registering users using djoser and secured menu endpoints

--- littlelemon/restaurant/urls.py
+++ littlelemon/restaurant/urls.py
@@ -2,12 +2,14 @@ from django.contrib import admin
 from django.urls import path
 from rest_framework.routers import DefaultRouter
 from .views import say_hello, index, SingleMenuItemView, MenuItemView, BookingViewSet
+from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
     path('hello', say_hello, name='say_hello'),
     path('', index, name="index"),
     path('menu/', MenuItemView.as_view()),
     path('menu/<int:pk>', SingleMenuItemView.as_view()),    
+    path('api-token-auth/', obtain_auth_token),
 ]
 
 router = DefaultRouter()
--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from rest_framework import generics, viewsets
+from rest_framework import generics, viewsets, routers
 from .models import Booking, Menu
 from .serializer import MenuSerializer, BookingSerializer
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
 
 
 def say_hello(request):
@@ -11,16 +13,22 @@ def say_hello(request):
 def index(request):
     return render(request, 'index.html', {})
 
+
 class MenuItemView(generics.ListCreateAPIView):
     queryset = Menu.objects.all()
     serializer_class = MenuSerializer
+    permission_classes = [IsAuthenticated]
+
 
 class SingleMenuItemView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Menu.objects.all()
     serializer_class = MenuSerializer
     lookup_field = 'pk'
+    permission_classes = [IsAuthenticated]
 
 class BookingViewSet(viewsets.ModelViewSet):
     queryset = Booking.objects.all()
     serializer_class = BookingSerializer
+
+
 
