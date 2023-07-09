f0a04afac946d40e62853221b832b1cdb7611065
Piotr Gebski <gebskipiotr@gmail.com>
2023-07-09 14:49:18 +0200

    Added api endpoints using rest_framework

--- littlelemon/restaurant/urls.py
+++ littlelemon/restaurant/urls.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
 from django.urls import path
-from .views import say_hello, index
+from rest_framework.routers import DefaultRouter
+from .views import say_hello, index, SingleMenuItemView, MenuItemView, BookingViewSet
 
 urlpatterns = [
     path('hello', say_hello, name='say_hello'),
-    path('', index, name="index")
+    path('', index, name="index"),
+    path('menu/', MenuItemView.as_view()),
+    path('menu/<int:pk>', SingleMenuItemView.as_view()),    
 ]
+
+router = DefaultRouter()
+router.register(r'tables', BookingViewSet)
--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from rest_framework import generics, viewsets
+from .models import Booking, Menu
+from .serializer import MenuSerializer, BookingSerializer
 
 
 def say_hello(request):
@@ -7,3 +10,17 @@ def say_hello(request):
 
 def index(request):
     return render(request, 'index.html', {})
+
+class MenuItemView(generics.ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+class SingleMenuItemView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+    lookup_field = 'pk'
+
+class BookingViewSet(viewsets.ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
+
