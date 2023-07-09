22660c86001d917c7eb0db4a60a7be24ce9f4058
Piotr Gebski <gebskipiotr@gmail.com>
2023-07-09 13:56:21 +0200

    added models for booking and menu

--- littlelemon/restaurant/urls.py
+++ littlelemon/restaurant/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from .views import say_hello, index
 
 urlpatterns = [
-    path('/hello', say_hello, name='say_hello'),
+    path('hello', say_hello, name='say_hello'),
     path('', index, name="index")
 ]
